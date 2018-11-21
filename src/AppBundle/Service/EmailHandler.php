e30b0f9f84c1e39aaec6bb669b28a698b5198176
Mayra Villarrubia <mayravillarrubia@gmail.com>
2018-11-21 20:50:32 -0300

    envio de mail al que solicita seguir una biblioteca

---
+++ src/AppBundle/Service/EmailHandler.php
@@ -0,0 +1,29 @@
+<?php
+namespace AppBundle\Service;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/*
+ * Manejador de envios de emails
+ */
+class EmailHandler extends Controller{
+
+    protected $container;
+
+    public function __construct($container) {
+        $this->container = $container;
+    }
+
+    public function getMessage($email, $user, $library, $subject, $view='email_solicitation') {
+        $message = \Swift_Message::newInstance()
+                ->setSubject($subject)
+                ->setFrom([$this->container->getParameter('mailer_user') => "Devp"])
+                ->setTo($email)
+                ->setBody($this->renderView('email/'.$view.'.html.twig', array(
+                            'user' => $reader,
+                            'library' => $library,
+                            )),
+                    'text/html');
+        return $message;
+    }
+}
