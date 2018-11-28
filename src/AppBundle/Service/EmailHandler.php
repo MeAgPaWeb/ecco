f3108812bc27055e7c25548ad0448a2837d873b2
Mayra Villarrubia <mayravillarrubia@gmail.com>
2018-11-28 17:51:06 -0300

    envio de mails cuando la solicitud fue aceptada/rechazada

--- src/AppBundle/Service/EmailHandler.php
+++ src/AppBundle/Service/EmailHandler.php
@@ -14,14 +14,13 @@ class EmailHandler extends Controller{
         $this->container = $container;
     }
 
-    public function getMessage($email, $user, $library, $message, $subject, $view='email_solicitation') {
+    public function getMessage($email, $user, $message, $subject, $view='email_solicitation') {
         $message = \Swift_Message::newInstance()
                 ->setSubject($subject)
                 ->setFrom([$this->container->getParameter('mailer_user') => "Devp"])
                 ->setTo($email)
                 ->setBody($this->renderView('email/'.$view.'.html.twig', array(
                             'user' => $user,
-                            'library' => $library,
                             'message' => $message
                             )),
                     'text/html');
